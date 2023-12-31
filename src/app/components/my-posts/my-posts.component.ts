769ceca29492e8ee3f6744f3fd65df17638584cb
prachisingh2 <68850178+prachisingh2@users.noreply.github.com>
2023-12-31 15:24:59 +0530

    Add files via upload

--- src/app/components/my-posts/my-posts.component.ts
+++ src/app/components/my-posts/my-posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from '../../services/api.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-my-posts',
@@ -8,31 +9,43 @@ import { ApiService } from '../../services/api.service';
   styleUrls: ['./my-posts.component.css']
 })
 export class MyPostsComponent implements OnInit {
-  myPost:any;
-  constructor(private restApi:ApiService,private route:Router) {
-    let uname=localStorage.getItem('user');
-    if(uname===""){
+  myPost: any;
+
+  constructor(
+    private restApi: ApiService,
+    private route: Router,
+    private authService: AuthService
+  ) {
+    let uname = this.authService.currentUser;
+    if (!uname) {
       this.route.navigate(['login']);
     }
-   }
+  }
 
   ngOnInit(): void {
     this.getMyPost();
   }
-  getMyPost(){
-   
-    this.restApi.getMyPost(localStorage.getItem('user_id')).subscribe(res=>{
-      this.myPost=res;
-    })
-  }
-  deletePost(i:number){
-    let res=confirm("Are you sure you want to delete this post?");
-    if(res){
-    this.restApi.deletePost(i).subscribe(res=>
-      { this.getMyPost();
-        alert("Post Deleted");
-    })
+
+  getMyPost() {
+    const currentUserId = this.authService.currentUserId;
+    //console.log(this.authService.currentUserId);
+    if (currentUserId) {
+        this.restApi.getMyPost(currentUserId).subscribe(res => {
+            this.myPost = res;
+            //console.log(this.myPost);  
+        });
+    } else {
+      this.route.navigate(['login']);
+    }
   }
 
-}
+  deletePost(i: number) {
+    let res = confirm("Are you sure you want to delete this post?");
+    if (res) {
+      this.restApi.deletePost(i).subscribe(res => {
+        this.getMyPost();
+        alert("Post Deleted");
+      })
+    }
+  }
 }
