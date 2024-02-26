7f77d4c73d5d57738a6eba47123115d2a915c612
prachisingh2 <68850178+prachisingh2@users.noreply.github.com>
2024-02-26 07:10:45 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { ThemeService } from './services/theme.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'blApp';
+
+  constructor(
+    private themeService: ThemeService,
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: Object 
+  ) {}
+
+  ngOnInit() {
+    this.themeService.getDarkMode().subscribe(isDarkModeEnabled => {
+      if (isPlatformBrowser(this.platformId)) {
+        if (isDarkModeEnabled) {
+          document.body.classList.add('dark-mode');
+        } else {
+          document.body.classList.remove('dark-mode');
+        }
+      }
+    });
+  }
+
+  toggleDarkMode() {
+    if (isPlatformBrowser(this.platformId)) {
+      const isDarkMode = !document.body.classList.contains('dark-mode');
+      this.themeService.setDarkMode(isDarkMode);
+      console.log('Dark mode is now:', isDarkMode);
+    }
+  }
 }
